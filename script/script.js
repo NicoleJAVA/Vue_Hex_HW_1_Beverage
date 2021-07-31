04d5cfbd7169f772608a49ab7e7401bd23fdc2ab
Nicole Chang <powernicole2@yahoo.com.tw>
2021-07-31 13:17:14 +0800

    refactor: extract js code as an independent file

---
+++ script/script.js
@@ -0,0 +1,142 @@
+const App = {
+    data() {
+      return {
+        tempProduct: {},
+        iceType: ['正常冰', '少冰', '微冰', '去冰', '熱'],
+        sugarType: ['全糖', '七分', '半糖', '三分', '無糖'],
+        toppingsType: ['珍珠', '粉條', '小粉圓', '椰果', '芋頭'],
+        products: [
+          {
+            name: '珍珠鮮奶茶',
+            engName: 'Pearl Milk Tea',
+            price: 60,
+            defaults: {
+              toppings: ['珍珠'],
+              sugar: '',
+              ice: '',
+            }
+          },
+          {
+            name: '椰果鮮奶茶',
+            engName: 'Coconut Milk Tea',
+            price: 60,
+            defaults: {
+              toppings: ['椰果'],
+              sugar: '',
+              ice: '',
+            }
+          },
+          {
+            name: '鮮奶茶',
+            engName: 'Milk Tea',
+            price: 50,
+            defaults: {
+              toppings: [''],
+              sugar: '',
+              ice: '',
+            }
+          },
+          {
+            name: '古意冬瓜茶 (糖固定)',
+            engName: 'Winter Melon Drink',
+            price: 30,
+            defaults: {
+              toppings: [''],
+              sugar: '全糖',
+              ice: '',
+            }
+          },
+          {
+            name: '蜜香紅茶',
+            engName: 'Black Tea',
+            price: 30,
+            defaults: {
+              toppings: [''],
+              sugar: '',
+              ice: '',
+            }
+          },
+          {
+            name: '包種青茶',
+            engName: 'Black Tea',
+            price: 35,
+            defaults: {
+              toppings: [''],
+              sugar: '',
+              ice: '',
+            }
+          },
+          {
+            name: '檸檬烏龍',
+            engName: 'Lemon Oolong Tea',
+            price: 55,
+            defaults: {
+              toppings: [''],
+              sugar: '',
+              ice: '',
+            }
+          },
+          {
+            name: '薑母茶 (熱飲)',
+            engName: 'Ginger Tea',
+            price: 55,
+            defaults: {
+              toppings: [''],
+              sugar: '',
+              ice: '熱',
+            }
+          },
+          {
+            name: '青草茶',
+            engName: 'Herbal Tea',
+            price: 35,
+            defaults: {
+              toppings: [''],
+              sugar: '',
+              ice: '',
+            }
+          },
+          {
+            name: '金桔檸檬',
+            engName: 'Kumquat Lemonade',
+            price: 40,
+            defaults: {
+              toppings: [''],
+              sugar: '',
+              ice: '',
+            }
+          },
+          {
+            name: '柳澄青茶',
+            engName: 'Orange Mountain Tea',
+            price: 45,
+            defaults: {
+              toppings: [''],
+              sugar: '',
+              ice: '',
+            }
+          },
+        ],
+      }
+    },
+  
+    methods: {
+        selectProduct(product) {
+            this.tempProduct = this.createProduct(product);
+        },
+  
+        createProduct(product) {
+            defaults = product.defaults;
+  
+            return {
+                count: '1',
+                ice: defaults.ice ? defaults.ice : this.iceType[0],
+                sugar: defaults.sugar ? defaults.sugar : this.sugarType[0],
+                toppings: [],
+                ... product
+            };
+        }
+    }
+  };
+  
+  Vue.createApp(App).mount('#app');
